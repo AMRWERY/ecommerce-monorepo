dd5e1b50a3a6e5828fc98a9b38563a0dca40db06
AMRWERY <amrmounir2@gmail.com>
2026-09-17 12:58:13 +0300

    .

--- apps/storefront/app/pages/index.vue
+++ apps/storefront/app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <lazy-hero-section />
-    
+
     <lazy-new-arrivals />
 
     <lazy-top-selling />
@@ -13,16 +13,12 @@
 </template>
 
 <script lang="ts" setup>
-definePageMeta({
-  alias: ['/contact'],
-})
-
 useHead({
-  title: 'Contact Us — SHOP.CO',
+  title: 'SHOP.CO',
   meta: [
     {
       name: 'description',
-      content: 'Contact us at SHOP.CO for any inquiries, support requests, or feedback. We’re here to help!',
+      content: 'SHOP.CO - Discover the latest trends in fashion, including new arrivals, best sellers, and exclusive collections.',
     },
   ],
 })
